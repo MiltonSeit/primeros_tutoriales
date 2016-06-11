f7bbe58aae4f9b1518b5c2e8fd77fe827ab69f32
MiltonSeit <milton0330@hotmail.com>
2016-06-11 15:00:16 -0300

    Terminado de html para desarrolladores

--- tutoriales/models.py
+++ tutoriales/models.py
@@ -8,7 +8,15 @@ class Tutoriales(models.Model):
 	titulo = models.CharField(max_length=100)
 	direccion = models.URLField(blank=True, null=True)
 	capitulo = models.IntegerField(blank=True, null=True)
-	#video = models.FileField(upload_to='documents/%Y/%m/%d')
+
+
+	def __str__(self):
+		return self.titulo
+
+class Descargas(models.Model):
+	titulo = models.CharField(max_length=100)
+	direccion = models.URLField(blank=True, null=True)
+	capitulo = models.IntegerField(blank=True, null=True)
 
 	def __str__(self):
 		return self.titulo
