357ef1219a8996f985a115f566f91eed658f5103
MiltonSeit <milton0330@hotmail.com>
2016-06-11 11:11:29 -0300

    correccion  de la DB cambio de campos

--- pagina_web/urls.py
+++ pagina_web/urls.py
@@ -6,12 +6,7 @@ from tutoriales import views
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT
-    }),
-    url(r'^documents/(?P<path>.*)', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT
-    }),
+    
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.principal, name ='principal'),
     url(r'^html', views.html, name ='html'),
--- tutoriales/models.py
+++ tutoriales/models.py
@@ -6,8 +6,9 @@ from django.db import models
 
 class Tutoriales(models.Model):
 	titulo = models.CharField(max_length=100)
-	video = models.FileField(upload_to='documents/%Y/%m/%d')
+	direccion = models.URLField(blank=True, null=True)
 	capitulo = models.IntegerField(blank=True, null=True)
+	#video = models.FileField(upload_to='documents/%Y/%m/%d')
 
 	def __str__(self):
 		return self.titulo
